438fc7e3d2058f01967f446448ccd475c93854de
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2023-12-31 12:04:01 +0300

    Update .gitignore and settings.py to have connection with PostgreSQL.

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local apps
+    'network',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'network',
+        'USER': 'postgres',
+        'PASSWORD': 'luka',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
