61be7da4043a01fc31bf58b58382c84b7b35b0f5
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-01-01 20:02:48 +0300

    Add admin function to be able to clear dept for supplier.

--- network/admin.py
+++ network/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-
-from network.models import Factory, Retailer, IndivodualSeller
+from django.db.models import F
+from network.models import Factory, Retailer, IndividualSeller
 
 
 @admin.register(Factory.Products)
 class ProductsAdmin(admin.ModelAdmin):
-    list_display = ['name', 'model', 'release_date']
+    list_display = ['id', 'name', 'model', 'release_date']
     list_filter = ['release_date', 'created_at']
     search_fields = ['name', 'model']
 
@@ -21,22 +21,44 @@ class FactoryAdmin(admin.ModelAdmin):
 
 
 @admin.register(Retailer)
-class ReatailerAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'email', 'country', 'city', 'street', 'house_number', 'created_at', 'supplier',
-                    'dept_to_supplier']
+class RetailerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'email', 'country', 'city', 'street', 'house_number', 'created_at',
+                    'dept_to_supplier', 'get_supplier']
     list_filter = ['created_at', 'products', 'country', 'city', 'street', 'house_number']
     search_fields = ['name', 'email', 'country', 'city', 'street', 'house_number']
     prepopulated_fields = {'slug': ('name',)}
     date_hierarchy = 'created_at'
     ordering = ['created_at', ]
+    actions = ['clear_debt']
+
+    def get_supplier(self, obj):
+        return obj.supplier.name
+
+    get_supplier.short_description = 'Supplier'
+
+    def clear_debt(self, _, queryset):
+        queryset.update(dept_to_supplier=F('dept_to_supplier') * 0)
+
+    clear_debt.short_description = "Clear Debt to Supplier For Selected Retailers"
 
 
-@admin.register(IndivodualSeller)
-class IndivodualSellerAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'email', 'country', 'city', 'street', 'house_number', 'created_at', 'supplier',
-                    'dept_to_supplier']
+@admin.register(IndividualSeller)
+class IndividualSellerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'email', 'country', 'city', 'street', 'house_number', 'created_at',
+                    'dept_to_supplier', 'get_supplier']
     list_filter = ['created_at', 'products', 'country', 'city', 'street', 'house_number']
     search_fields = ['name', 'email', 'country', 'city', 'street', 'house_number']
     prepopulated_fields = {'slug': ('name',)}
     date_hierarchy = 'created_at'
     ordering = ['created_at', ]
+    actions = ['clear_debt']
+
+    def get_supplier(self, obj):
+        return obj.supplier.name
+
+    get_supplier.short_description = 'Supplier'
+
+    def clear_debt(self, _, queryset):
+        queryset.update(dept_to_supplier=F('dept_to_supplier') * 0)
+
+    clear_debt.short_description = "Clear Debt to Supplier For Selected Individual Sellers"
