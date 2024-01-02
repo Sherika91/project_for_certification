f75799e0722c7504fcf9b3d739a496eccb6742b7
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-01-03 01:16:52 +0300

    Created user for interacting with project

---
+++ users/permissions.py
@@ -0,0 +1,19 @@
+from rest_framework import permissions
+
+
+class IsOwnerOrAdmin(permissions.BasePermission):
+    """ Custom permission to only allow owners of an object to edit it. """
+
+    def has_object_permission(self, request, view, obj):
+        if request.user == obj.owner or request.user.is_staff:
+            return True
+        return False
+
+
+class IsAdmin(permissions.BasePermission):
+    """ Custom permission to only allow admins to access. """
+
+    def has_permission(self, request, view):
+        if request.user.is_staff:
+            return True
+        return False
--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from users.models import User
+from users.permissions import IsOwnerOrAdmin, IsAdmin
+from users.serializers import UserSerializer
+
+
+class UsersViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    default_permission = [AllowAny]
+    permissions = {
+        'list': [IsAdmin],
+        'retrieve': [IsAuthenticated, IsOwnerOrAdmin],
+        'create': [],
+        'update': [IsOwnerOrAdmin],
+        'partial_update': [IsOwnerOrAdmin],
+        'destroy': [IsOwnerOrAdmin],
+    }
+
+    def get_permissions(self):
+        return [permission() for permission in self.permissions.get(self.action, self.default_permission)]
