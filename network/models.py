5fcc02d565e0e5049ba5460962353e01788c50eb
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-01-03 01:18:22 +0300

    models rework

--- network/models.py
+++ network/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 NULLABLE = {
     'null': True,
@@ -6,92 +7,66 @@ NULLABLE = {
 }
 
 
-class Factory(models.Model):
-    name = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    model = models.CharField(max_length=50)
+    release_date = models.DateField()
+    owner = models.ForeignKey('Network', on_delete=models.CASCADE)
+    date_created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}, {self.model}, {self.release_date}"
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+        ordering = ['-date_created']
+        indexes = [
+            models.Index(fields=['name', 'model', 'release_date'])
+        ]
+
+
+class Network(MPTTModel):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
     email = models.EmailField()
     country = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     street = models.CharField(max_length=100)
-    house_number = models.CharField(max_length=150)
-    created_at = models.DateTimeField(auto_now_add=True)
-    products = models.ManyToManyField('Products', related_name='factory_products')
-
-    class Products(models.Model):
-        name = models.CharField(max_length=250)
-        model = models.CharField(max_length=250)
-        release_date = models.DateField()
-        created_at = models.DateTimeField(auto_now_add=True)
-        updated_at = models.DateTimeField(auto_now=True)
-
-        def __str__(self):
-            return f"{self.name}, {self.model}, {self.release_date}"
-
-        class Meta:
-            verbose_name = 'Products'
-            verbose_name_plural = 'Products'
-            ordering = ['-created_at']
-            indexes = [
-                models.Index(fields=['name', 'model', 'release_date'])
-            ]
+    house_number = models.CharField(max_length=50)
+    dept_to_supplier = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    parent = TreeForeignKey('self', **NULLABLE, related_name='childeren', db_index=True, on_delete=models.CASCADE)
+    date_created = models.DateField(auto_now_add=True)
+    products = models.ManyToManyField(Product, related_name='products')
 
     def __str__(self):
         return f"{self.name}, {self.email}, {self.country}"
 
     class Meta:
-        verbose_name = 'Factory'
-        verbose_name_plural = 'Factories'
-        ordering = ['-created_at']
+        verbose_name = 'Network'
+        verbose_name_plural = 'Networks'
+        ordering = ['-date_created']
         indexes = [
             models.Index(fields=['name', 'slug', 'email', 'country', 'city']),
         ]
 
 
-class Retailer(models.Model):
-    name = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
-    email = models.EmailField()
-    country = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
-    street = models.CharField(max_length=100)
-    house_number = models.CharField(max_length=150)
-    created_at = models.DateTimeField(auto_now_add=True)
-    supplier = models.ForeignKey(Factory, on_delete=models.CASCADE, related_name='retailer_supplier')
-    dept_to_supplier = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
-    products = models.ManyToManyField(Factory.Products, related_name='retailer_products')
+class Factory(Network):
+    class Meta:
+        verbose_name = 'Factory'
+        verbose_name_plural = 'Factories'
+        ordering = ['-date_created']
 
-    def __str__(self):
-        return f"{self.name}, {self.email}, {self.country}"
 
+class Retailer(Network):
     class Meta:
         verbose_name = 'Retailer'
         verbose_name_plural = 'Retailers'
-        ordering = ['created_at']
-        indexes = [
-            models.Index(fields=['name', 'slug', 'email', 'country', 'city'])
-        ]
+        ordering = ['-date_created']
 
 
-class IndivodualSeller(models.Model):
-    name = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
-    email = models.EmailField()
-    country = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
-    street = models.CharField(max_length=100)
-    house_number = models.CharField(max_length=150)
-    created_at = models.DateTimeField(auto_now_add=True)
-    supplier = models.ForeignKey(Retailer, on_delete=models.CASCADE, related_name='individual_seller_supplier')
-    dept_to_supplier = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
-    products = models.ManyToManyField(Factory.Products, related_name='indivodual_seller_products')
-
-    def __str__(self):
-        return f"{self.name}, {self.email}, {self.country}, {self.city}"
-
+class IndividualSeller(Network):
     class Meta:
-        verbose_name = 'IndivodualSeller'
-        verbose_name_plural = 'IndivodualSellers'
-        ordering = ['-created_at']
-        indexes = [
-            models.Index(fields=['name', 'slug', 'email', 'country', 'city'])
-        ]
+        verbose_name = 'Individual Seller'
+        verbose_name_plural = 'Individual Sellers'
+        ordering = ['-date_created']
