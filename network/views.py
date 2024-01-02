c068921b912bf159db28e7f8c13a32c2743b0d2f
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-01-03 01:17:27 +0300

    search fields add for Retailer

--- network/views.py
+++ network/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
 from .models import Retailer
 from .permissions import UserIsActive
 from .serializers import RetailerSerializer
@@ -8,5 +10,5 @@ class RetailerViewSet(viewsets.ModelViewSet):
     queryset = Retailer.objects.all()
     serializer_class = RetailerSerializer
     filter_backends = [filters.SearchFilter]
-    search_fields = ['country']
-    permission_classes = [UserIsActive]
+    search_fields = ['country', 'city', 'street', 'name']
+    permission_classes = [UserIsActive, IsAuthenticated]
