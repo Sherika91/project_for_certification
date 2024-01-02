396b5dc92b4fe302e4eee56479deb1653267590a
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-01-03 01:18:02 +0300

    add minor rework for better viewing of api.

--- network/serializers.py
+++ network/serializers.py
@@ -3,8 +3,11 @@ from network.models import Retailer
 
 
 class RetailerSerializer(serializers.ModelSerializer):
+    products = serializers.StringRelatedField(many=True, read_only=True)
+    supplier = serializers.StringRelatedField(read_only=True)
+
     class Meta:
         model = Retailer
-        fields = ['id', 'name', 'slug', 'email', 'country', 'city', 'street', 'house_number', 'created_at', 'supplier',
+        fields = ['id', 'name', 'slug', 'email', 'country', 'city', 'street', 'house_number', 'date_created', 'supplier',
                   'dept_to_supplier', 'products']
         read_only_fields = ['dept_to_supplier']
