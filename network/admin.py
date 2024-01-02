64b5221b72e178aa729099dec6663352b733a77c
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-01-03 01:16:02 +0300

    Changes in Models and hierarchy, aslo added admin models for displaying all the information to admin, also add func so admin is able to clear dept for obj

--- network/admin.py
+++ network/admin.py
@@ -1,40 +1,34 @@
 from django.contrib import admin
 from django.db.models import F
-from network.models import Factory, Retailer, IndividualSeller
+
+from .models import Factory, Product, Retailer, IndividualSeller
 
 
-@admin.register(Factory.Products)
+@admin.register(Product)
 class ProductsAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'model', 'release_date']
-    list_filter = ['release_date', 'created_at']
+    list_display = ['id', 'name', 'model', 'release_date', 'date_created', 'owner']
+    list_filter = ['release_date', 'date_created']
     search_fields = ['name', 'model']
 
 
 @admin.register(Factory)
 class FactoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'email', 'country', 'city', 'street', 'house_number', 'created_at']
-    list_filter = ['created_at', 'products', 'country', 'city', 'street', 'house_number']
+    list_display = ['name', 'slug', 'email', 'country', 'city', 'street', 'house_number', 'date_created']
+    list_filter = ['country', 'city', 'street', 'house_number', 'date_created']
     search_fields = ['name', 'email', 'country', 'city', 'street', 'house_number']
     prepopulated_fields = {'slug': ('name',)}
-    date_hierarchy = 'created_at'
-    ordering = ['created_at', ]
+    date_hierarchy = 'date_created'
+    ordering = ['date_created', ]
 
 
 @admin.register(Retailer)
-class RetailerAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'email', 'country', 'city', 'street', 'house_number', 'created_at',
-                    'dept_to_supplier', 'get_supplier']
-    list_filter = ['created_at', 'products', 'country', 'city', 'street', 'house_number']
+class FactoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'email', 'country', 'city', 'street', 'house_number', 'date_created']
+    list_filter = ['country', 'city', 'street', 'house_number', 'date_created']
     search_fields = ['name', 'email', 'country', 'city', 'street', 'house_number']
     prepopulated_fields = {'slug': ('name',)}
-    date_hierarchy = 'created_at'
-    ordering = ['created_at', ]
-    actions = ['clear_debt']
-
-    def get_supplier(self, obj):
-        return obj.supplier.name
-
-    get_supplier.short_description = 'Supplier'
+    date_hierarchy = 'date_created'
+    ordering = ['date_created', ]
 
     def clear_debt(self, _, queryset):
         queryset.update(dept_to_supplier=F('dept_to_supplier') * 0)
@@ -43,20 +37,13 @@ class RetailerAdmin(admin.ModelAdmin):
 
 
 @admin.register(IndividualSeller)
-class IndividualSellerAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'email', 'country', 'city', 'street', 'house_number', 'created_at',
-                    'dept_to_supplier', 'get_supplier']
-    list_filter = ['created_at', 'products', 'country', 'city', 'street', 'house_number']
+class FactoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'email', 'country', 'city', 'street', 'house_number', 'date_created']
+    list_filter = ['country', 'city', 'street', 'house_number', 'date_created']
     search_fields = ['name', 'email', 'country', 'city', 'street', 'house_number']
     prepopulated_fields = {'slug': ('name',)}
-    date_hierarchy = 'created_at'
-    ordering = ['created_at', ]
-    actions = ['clear_debt']
-
-    def get_supplier(self, obj):
-        return obj.supplier.name
-
-    get_supplier.short_description = 'Supplier'
+    date_hierarchy = 'date_created'
+    ordering = ['date_created', ]
 
     def clear_debt(self, _, queryset):
         queryset.update(dept_to_supplier=F('dept_to_supplier') * 0)
