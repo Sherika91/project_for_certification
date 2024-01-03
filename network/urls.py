f909053c6dec855e468e57d7197f5c9ecaef25e8
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-01-03 12:33:41 +0300

    factory serializer add and view set for seller add

--- network/urls.py
+++ network/urls.py
@@ -8,6 +8,8 @@ app_name = NetworkConfig.name
 
 router = DefaultRouter()
 router.register(r'retailer', views.RetailerViewSet, basename='retailer')
+router.register(r'factory', views.FactoryViewSet, basename='factory')
+router.register(r'seller', views.IndividualSellerViewSet, basename='individual')
 
 urlpatterns = [
     path('', include(router.urls)),
